2bbcc8c72008ea162840ac5b3527a1632815ea2c
Ralf Gräf <graef.ralf@gmail.com>
2020-09-09 21:39:11 +0200

    completed half of the static lesson

---
+++ resources/views/info.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Info</div>
+
+                <div class="card-body">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore nemo corrupti odit eius? Cumque laboriosam repudiandae voluptas doloribus, hic quasi, voluptates fugiat neque in commodi tenetur omnis. Omnis, possimus modi.
+
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab quidem ea quis impedit eveniet, laudantium officiis incidunt ullam in totam adipisci eligendi perspiciatis natus quod, quam iure laborum! Tempora, autem?
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/starting_page.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Starting Page</div>
+
+                <div class="card-body">
+                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis ipsum, magnam minus perferendis cum nesciunt consequatur incidunt aliquam quos unde! Accusamus atque dolore totam expedita vel et tenetur cumque accusantium.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
